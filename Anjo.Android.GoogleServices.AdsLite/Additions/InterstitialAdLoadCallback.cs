5041f7bd7cdbc56587910aacd238b4a83386c484
Mhd Wael Anjo <wael.tcc.anjo@gmail.com>
2023-09-25 10:39:56 +0300

    22.0.0

---
+++ Anjo.Android.GoogleServices.AdsLite/Additions/InterstitialAdLoadCallback.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Android.Runtime;
+using Com.Google.Android.Gms.Ads.Appopen;
+using Com.Google.Android.Gms.Ads.Interstitial;
+using Com.Google.Android.Gms.Ads.Rewarded;
+using Com.Google.Android.Gms.Ads.Rewardedinterstitial;
+
+namespace Anjo.Android.GoogleAds
+{
+    public abstract class AnjoInterstitialLoadCallback : Com.Google.Android.Gms.Ads.Interstitial.InterstitialAdLoadCallback
+    {
+        private static Delegate cb_onAdLoaded;
+
+        private static Delegate GetOnInterstitialAdLoadedHandler()
+        {
+            if (cb_onAdLoaded is null)
+            {
+                cb_onAdLoaded = JNINativeWrapper.CreateDelegate((Action<IntPtr, IntPtr, IntPtr>)n_onAdLoaded);
+            }
+            return cb_onAdLoaded;
+        }
+
+        private static void n_onAdLoaded(IntPtr jnienv, IntPtr native__this, IntPtr native_p0)
+        {
+            AnjoInterstitialLoadCallback thisobject = GetObject<AnjoInterstitialLoadCallback>(jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+            InterstitialAd resultobject = GetObject<InterstitialAd>(native_p0, JniHandleOwnership.DoNotTransfer);
+            thisobject.OnAdLoaded(resultobject);
+        }
+
+        [Register("onAdLoaded", "(Lcom/google/android/gms/ads/interstitial/InterstitialAd;)V", "GetOnInterstitialAdLoadedHandler")]
+        public virtual void OnAdLoaded(InterstitialAd p0)
+        {
+
+        }
+    }
+
+    public abstract class AnjoRewardedLoadCallback : Com.Google.Android.Gms.Ads.Rewarded.RewardedAdLoadCallback
+    {
+        private static Delegate cb_onAdLoaded;
+
+        private static Delegate GetOnRewardedAdLoadedHandler()
+        {
+            if (cb_onAdLoaded is null)
+            {
+                cb_onAdLoaded = JNINativeWrapper.CreateDelegate((Action<IntPtr, IntPtr, IntPtr>)n_onAdLoaded);
+            }
+            return cb_onAdLoaded;
+        }
+
+        private static void n_onAdLoaded(IntPtr jnienv, IntPtr native__this, IntPtr native_p0)
+        {
+            AnjoRewardedLoadCallback thisobject = GetObject<AnjoRewardedLoadCallback>(jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+            RewardedAd resultobject = GetObject<RewardedAd>(native_p0, JniHandleOwnership.DoNotTransfer);
+            thisobject.OnAdLoaded(resultobject);
+        }
+
+        [Register("onAdLoaded", "(Lcom/google/android/gms/ads/rewarded/RewardedAd;)V", "GetOnRewardedAdLoadedHandler")]
+        public virtual void OnAdLoaded(RewardedAd p0)
+        {
+
+        }
+    }
+
+
+    public abstract class AnjoAppOpenLoadCallback : AppOpenAd.AppOpenAdLoadCallback
+    {
+        private static Delegate cb_onAdLoaded;
+
+        private static Delegate GetOnAppOpenAdLoadedHandler()
+        {
+            if (cb_onAdLoaded is null)
+            {
+                cb_onAdLoaded = JNINativeWrapper.CreateDelegate((Action<IntPtr, IntPtr, IntPtr>)n_onAdLoaded);
+            }
+            return cb_onAdLoaded;
+        }
+
+        private static void n_onAdLoaded(IntPtr jnienv, IntPtr native__this, IntPtr native_p0)
+        {
+            AnjoAppOpenLoadCallback thisobject = GetObject<AnjoAppOpenLoadCallback>(jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+            AppOpenAd resultobject = GetObject<AppOpenAd>(native_p0, JniHandleOwnership.DoNotTransfer);
+            thisobject.OnAdLoaded(resultobject);
+        }
+
+        [Register("onAdLoaded", "(Lcom/google/android/gms/ads/appopen/AppOpenAd;)V", "GetOnAppOpenAdLoadedHandler")]
+        public virtual void OnAdLoaded(AppOpenAd p0)
+        {
+
+        }
+    }
+
+    public abstract class AnjoRewardedInterstitialLoadCallback : Com.Google.Android.Gms.Ads.Rewardedinterstitial.RewardedInterstitialAdLoadCallback
+    {
+        private static Delegate cb_onAdLoaded;
+
+        private static Delegate GetOnRewardedInterstitialAdLoadedHandler()
+        {
+            if (cb_onAdLoaded is null)
+            {
+                cb_onAdLoaded = JNINativeWrapper.CreateDelegate((Action<IntPtr, IntPtr, IntPtr>)n_onAdLoaded);
+            }
+            return cb_onAdLoaded;
+        }
+
+        private static void n_onAdLoaded(IntPtr jnienv, IntPtr native__this, IntPtr native_p0)
+        {
+            AnjoRewardedInterstitialLoadCallback thisobject = GetObject<AnjoRewardedInterstitialLoadCallback>(jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+            RewardedInterstitialAd resultobject = GetObject<RewardedInterstitialAd>(native_p0, JniHandleOwnership.DoNotTransfer);
+            thisobject.OnAdLoaded(resultobject);
+        }
+
+        [Register("onAdLoaded", "(Lcom/google/android/gms/ads/rewardedinterstitial/RewardedInterstitialAd;)V", "GetOnRewardedInterstitialAdLoadedHandler")]
+        public virtual void OnAdLoaded(RewardedInterstitialAd p0)
+        {
+
+        }
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # GoogleServices_22.0.0
+
+---------------------------------
+Google Play Services library SDK for .NET Android (formerly Xamarin.Android) bindings for AndroidX library - Wael Anjo
+---------------------------------
+
+The Google Mobile Ads SDK is the latest generation in Google mobile advertising, featuring refined ad formats and streamlined APIs for access to mobile ad networks and advertising solutions. 
+The SDK enables mobile app developers to maximize their monetization in native mobile apps.
+
+Quickstart for Android : https://developers.google.com/admob/android/quick-start
+
+Google Mobile Ads repository for Android : https://github.com/googleads/googleads-mobile-android-examples
+
+Star on Github if this project helps you: https://github.com/mhdwaelanjo/Anjo.Android.GoogleServices
+ 
+---------------------------------
+
+
+Supports fixing the problem of Loading ads using : 
+---------------------------------
+
+- Let's rename for Interstitial ads **InterstitialAdLoadCallback** to **AnjoInterstitialLoadCallback**.
+- Let's rename for Rewarded ads **RewardedAdLoadCallback** to **AnjoRewardedLoadCallback**.
+- Let's rename for App Open Ads **AppOpenAd.AppOpenAdLoadCallback** to **AnjoAppOpenLoadCallback**.
+- Let's rename for Rewarded interstitial ads **RewardedInterstitialAdLoadCallback** to **AnjoRewardedInterstitialLoadCallback**.
